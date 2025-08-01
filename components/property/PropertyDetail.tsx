716945c5196e6c9d91a39f205d310bf1505cfab4
PeterAwudi <awudipeter20@gmail.com>
2025-08-01 21:22:15 +0000

    Initial commit

--- components/property/PropertyDetail.tsx
+++ components/property/PropertyDetail.tsx
@@ -1,8 +1,23 @@
-// components/property/PropertyDetail.tsx
-import React from 'react';
+interface Property {
+  id: string;
+  title: string;
+  description: string;
+  // Add all other necessary fields
+}
 
-const PropertyDetail = () => {
-  return <div>Property Detail Page</div>;
+interface PropertyDetailProps {
+  property: Property;
+}
+
+const PropertyDetail = ({ property }: PropertyDetailProps) => {
+  return (
+    <div>
+      <h1 className="text-xl font-bold">{property.title}</h1>
+      <p>{property.description}</p>
+      {/* Render other property details here */}
+    </div>
+  );
 };
 
 export default PropertyDetail;
+
