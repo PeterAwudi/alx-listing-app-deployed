3ee1541b4d1506412d005e39d731bfdaff6758e7
PeterAwudi <awudipeter20@gmail.com>
2025-08-01 21:14:16 +0000

    Initial commit

--- pages/api/properties/index.ts
+++ pages/api/properties/index.ts
@@ -1,4 +1,6 @@
-export default function handler(req, res) {
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
   res.status(200).json([
     { id: "1", title: "Cozy Apartment", description: "Nice place", price: 120 },
     { id: "2", title: "Beach House", description: "Ocean view", price: 200 },
